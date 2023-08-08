110fd5dd72e1a604496098e867c606069a50d6de
reginaapo <regina_apodaca@ciencias.unam.mx>
2023-08-08 11:30:39 -0700

    Update main.py
    1) Made it easier to select the exact value that you want for the wavelength
    2) Fixed a bug with the angles of integration

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 # Calling Necessary packages
+import importlib.resources
+import numpy as np
+import matplotlib.pyplot as plt
+import miepython
+from scipy.integrate import quad
+from scipy.optimize import curve_fit
+from numpy import sin
+from numpy import cos
+from numpy import exp
 import random
 import tkinter
 from tkinter import *
@@ -21,11 +30,11 @@ from numpy import exp
 import os
 import os.path
 
-#Importing all relevant Refractive index data
-current_dir = os.path.dirname(__file__)
-
 # Construct the relative file path
+current_dir = os.path.dirname(__file__)
 file_path = os.path.join(current_dir, "RI")
+
+# Importing all relevant Refractive index data
 nnamesio2 = file_path+r"\refractiveIndexSiO2.csv"
 sio2 = np.genfromtxt(nnamesio2, delimiter=',', skip_header=1)
 sio2_lam = sio2[:, 0]
@@ -70,8 +79,8 @@ h2so4_mre95 = h2so495[:, 1]
 
 nname25 = file_path+r"\refractiveIndexSulfuricAcid25.txt"
 h2so425 = np.genfromtxt(nname25, delimiter='\t', skip_header=1)
-h2so4_lam25 = h2so425[:,0]
-h2so4_mre25 = h2so425[:,1]
+h2so4_lam25 = h2so425[:, 0]
+h2so4_mre25 = h2so425[:, 1]
 
 nname38 = file_path+r"\refractiveIndexSulfuricAcid38.txt"
 h2so438 = np.genfromtxt(nname38, delimiter='\t', skip_header=1)
@@ -80,45 +89,45 @@ h2so4_mre38 = h2so438[:, 1]
 
 nname5 = file_path+r"\refractiveIndexSulfuricAcid50.txt"
 h2so45 = np.genfromtxt(nname5, delimiter='\t', skip_header=1)
-h2so4_lam5 = h2so45[:,0]
-h2so4_mre50 = h2so45[:,1]
+h2so4_lam5 = h2so45[:, 0]
+h2so4_mre50 = h2so45[:, 1]
 
 
 nname6 = file_path+r"\refractiveIndexSulfuricAcid61.txt"
 h2so46 = np.genfromtxt(nname6, delimiter='\t', skip_header=1)
-h2so4_lam6 = h2so46[:,0]
-h2so4_mre60 = h2so46[:,1]
+h2so4_lam6 = h2so46[:, 0]
+h2so4_mre60 = h2so46[:, 1]
 
 
 nname75 = file_path+r"\refractiveIndexSulfuricAcid75.txt"
 h2so475 = np.genfromtxt(nname75, delimiter='\t', skip_header=1)
-h2so4_lam75 = h2so475[:,0]
-h2so4_mre75 = h2so475[:,1]
+h2so4_lam75 = h2so475[:, 0]
+h2so4_mre75 = h2so475[:, 1]
 
 
 nname84 = file_path+r"\refractiveIndexSulfuricAcid84.txt"
 h2so484 = np.genfromtxt(nname84, delimiter='\t', skip_header=1)
-h2so4_lam84= h2so484[:,0]
-h2so4_mre84 = h2so484[:,1]
+h2so4_lam84 = h2so484[:, 0]
+h2so4_mre84 = h2so484[:, 1]
 
 
 nname9 = file_path+r"\refractiveIndexSulfuricAcid90.txt"
 h2so49 = np.genfromtxt(nname9, delimiter='\t', skip_header=1)
-h2so4_lam9 = h2so49[:,0]
-h2so4_mre90 = h2so49[:,1]
+h2so4_lam9 = h2so49[:, 0]
+h2so4_mre90 = h2so49[:, 1]
 
-nname0 = file_path+ r"\refractiveIndexSulfuricAcid00.txt"
+nname0 = file_path + r"\refractiveIndexSulfuricAcid00.txt"
 h2o = np.genfromtxt(nname0, delimiter='\t', skip_header=1)
-h2o_lam = h2o[:,0]
-h2o_mre = h2o[:,1]
+h2o_lam = h2o[:, 0]
+h2o_mre = h2o[:, 1]
 
-#Defining necessary function
+# Defining necessary function for Mie Scattering Code
 def find_nearest(array, value):
    array = np.asarray(array)
    idx = (np.abs(array - value)).argmin()
    return array[idx]
 
-#Creating GUI
+# Creating GUI
 root = tkinter.Tk()
 tkinter.Tk.wm_title(root, "Mie Scattering Simulation")
 frame1 = tkinter.Frame(root)
@@ -147,9 +156,9 @@ L1S.pack( )
 def show():
    label.config(text=clickedS.get())
 # Dropdown menu options
-minerals=["H2O","H2SO4-25","H2SO4-38","H2SO4-50","H2SO4-75","H2SO4-84","H2SO4-95","SiO2","TiO2","Al2O3","FeO","MnO","CaO","K2O","MgO","Ph3","No Mineral"]
-mMineral=[h2o_mre,h2so4_mre25,h2so4_mre38,h2so4_mre50,h2so4_mre75,h2so4_mre84,h2so4_mre95,sio2_mre,tio2_mre,al2o3_mre,feo_mre,mno2_mre,cao_mre,0,mgo_mre,[2.144],0]
-lamMineral=[h2o_lam/1000,h2so4_lam25/1000,h2so4_lam38/1000,h2so4_lam5/1000,h2so4_lam75/1000,h2so4_lam84/1000,h2so4_lam95/1000,sio2_lam,tio2_lam,al2o3_lam,feo_lam,mno2_lam/1000,cao_lam/1000,0,mgo_lam,h2so4_lam95/1000,0]
+minerals = ["H2O", "H2SO4-25", "H2SO4-38", "H2SO4-50","H2SO4-75", "H2SO4-84", "H2SO4-95", "SiO2", "TiO2", "Al2O3", "FeO", "MnO", "CaO", "K2O", "MgO", "Ph3", "No Mineral"]
+mMineral = [h2o_mre, h2so4_mre25, h2so4_mre38, h2so4_mre50, h2so4_mre75, h2so4_mre84, h2so4_mre95, sio2_mre, tio2_mre, al2o3_mre, feo_mre, mno2_mre, cao_mre, 0, mgo_mre, [2.144], 0]
+lamMineral = [h2o_lam/1000, h2so4_lam25/1000, h2so4_lam38/1000, h2so4_lam5/1000, h2so4_lam75/1000, h2so4_lam84/1000, h2so4_lam95/1000, sio2_lam, tio2_lam, al2o3_lam, feo_lam, mno2_lam/1000, cao_lam/1000, 0, mgo_lam, h2so4_lam95/1000, 0]
 # datatype of menu text
 clickedS = StringVar()
 # initial menu text
@@ -158,15 +167,14 @@ clickedS.set("H2SO4-95")
 dropS = OptionMenu(frame2, clickedS, *minerals)
 dropS.pack()
 
-kmS=minerals.index(clickedS.get())
-kAerosol=mMineral[kmS]
-sAerosol=DoubleVar()
+kmS = minerals.index(clickedS.get())
+kAerosol = mMineral[kmS]
+sAerosol = DoubleVar()
 
 L1 = Label(frame2, text="Select a mineral to Simulate: ")
 L1.pack( )
 def show():
    label.config(text=clickedM.get())
-# Dropdown menu options
 
 # datatype of menu text
 clickedM = StringVar()
@@ -176,17 +184,18 @@ clickedM.set("No Mineral")
 drop = OptionMenu(frame2, clickedM, *minerals)
 drop.pack()
 if clickedM.get() == 'No Mineral':
-   km=minerals.index(clickedS.get())
+   km = minerals.index(clickedS.get())
 else:
-   km=minerals.index(clickedM.get())
-kMineral=mMineral[km]
-sMineral=DoubleVar()
+   km = minerals.index(clickedM.get())
+kMineral = mMineral[km]
+sMineral = DoubleVar()
 aS = DoubleVar()
-sWavelen=DoubleVar()
+sWavelen = DoubleVar()
 
+# Code for Particle Size conditions
 L2 = Label(frame2, text="Select if you want to graph a range or a specific value for the Particle Size (um): ")
 L2.pack()
-typeA=["Range","Value"]
+typeA = ["Range","Value"]
 clickedA = StringVar()
 clickedA.set("Value")
 var = DoubleVar()
@@ -237,7 +246,8 @@ def selA(clickedA):
 dropA = OptionMenu(frame2, clickedA, *typeA, command=selA)
 dropA.pack()
 
-
+# Define the Physical Parameters of the Experiment
+# Measured Angles
 anvarMin = DoubleVar()
 anvarMax = DoubleVar()
 anMinL = Label(frame1, text="Select minimum angle (°): ")
@@ -249,6 +259,7 @@ anMax =StringVar(frame1)
 anMax.set("173.1")
 anMaxS = Spinbox(frame1, from_=0, to=180, textvariable=anMax)
 
+# Window Radius and Focal Length
 winR = DoubleVar()
 focD = DoubleVar()
 winRL = Label(frame1, text="Select Window Radius (mm): ")
@@ -270,14 +281,16 @@ winRS.pack()
 focDL.pack()
 focDS.pack()
 
+# Defining the Wavelength
 L3 = Label(frame1, text="Select if you want to graph a range or a specific value for the Wavelength (nm): ")
 L3.pack()
 typeW=["Range","Value"]
 clickedW = StringVar()
 clickedW.set("Value")
 wvar = DoubleVar()
-wscale = Scale(frame1, label="Wavelength (nm):", variable=wvar, orient=HORIZONTAL, from_=400, to=620, resolution=0.1)
-wlabel = Label(frame1)
+wscale = Spinbox(frame1, from_=400, to=620,  increment=0.1, textvariable=wvar)
+#wscale = Scale(frame1, label="Wavelength (nm):", variable=wvar, orient=HORIZONTAL, from_=400, to=620, resolution=0.1)
+wlabel = Label(frame1, text="Wavelength (nm):")
 wvarMin = DoubleVar()
 wvarMax = DoubleVar()
 L31 = Label(frame1, text="Select minimum wavelength (nm): ")
@@ -321,6 +334,7 @@ def wRsel():
       if l in indJ:
          sMineral.append(kMineral[l])
          sAerosol.append(kAerosol[l])
+
 wbutton= Button(frame1, text="Apply", command=wsel)
 wRbutton = Button(frame1, text="Apply", command=wRsel)
 def selW(clickedW):
@@ -347,7 +361,7 @@ dropW = OptionMenu(frame1, clickedW, *typeW, command=selW)
 dropW.pack()
 
 
-
+# Graph Selection
 L4 = Label(frame3, text="Select the types of Graphs you want to visualize: ")
 L4.pack()
 
@@ -359,8 +373,7 @@ def isfloat(num):
    except TypeError:
       return False
 
-
-
+#Mie Scattering Code
 def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
    newWindow = Toplevel(root)
    newWindow.title("Mie Scattering Simulation Results")
@@ -377,13 +390,14 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
       elif isfloat(lambda0) == False:
          print('Selected Wavelength: (%s,%s)' % (min(lambda0), max(lambda0)))
    elif isfloat(a0) == False:
-      print('Selected Particle Size: (%s,%.1f)' % (min(a0), max(a0)))
+      print('Selected Particle Size: (%s,%.f)' % (min(a0), max(a0)))
       if isfloat(lambda0) == True:
          print('Selected Wavelength: %s' % lambda0)
       elif isfloat(lambda0) == False:
          print('Selected Wavelength: (%s,%s)' % (min(lambda0), max(lambda0)))
    # theta = np.linspace(-180,180,1800)
-   theta = np.arange(-181, 181, 1)
+   theta = np.arange(-181, 181, 0.1)
+   #theta = np.arange(eVec0[0], eVec0[1]+0.1, 0.1)
    numG = gVec0.count(True)
 
    if gVec0[0] == True:
@@ -426,7 +440,7 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
          print(m_sphere)
          m = m_sphere / mre
          if clickedM.get() == 'No Mineral':
-            m = m_sphere
+            m = mre
             qext, qsca, qback, g = miepython.mie(m, x)
          else:
             qext, qsca, qback, g = miepython.ez_mie(m_sphere, a, lam, mre)
@@ -479,7 +493,7 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
          # plt.show()
          def rawI(x):
             #tt = np.arange(-181, 181, 1)
-            tt = np.arange(eVec0[0], eVec0[1]+1, 1)
+            tt = np.arange(eVec0[0], eVec0[1]+0.1, .1)
             [k] = np.where(tt == x)
             if len(k) == 0:
                newTheta = find_nearest(tt, x)
@@ -489,7 +503,7 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
 
          def rawQ(x):
             #tt = np.arange(-181, 181, 1)
-            tt = np.arange(eVec0[0], eVec0[1]+1, 1)
+            tt = np.arange(eVec0[0], eVec0[1]+0.1, .1)
             [k] = np.where(tt == x)
             if len(k) == 0:
                newTheta = find_nearest(tt, x)
@@ -502,9 +516,11 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
          tt = np.arange(eVec0[0], eVec0[1]+0.1, 0.1)
          for f in tt:
             fl = eVec0[3]*1000  # Focal Length (200000 microns)
+            print(fl)
             midTheta = (max(tt) + min(tt)) / 2  # Mid Point Theta (rad)
             deltaTheta = np.abs(f - midTheta) * np.pi / 180  # Maximum delta (rad)
             windowR = eVec0[2]*1000   # Window radius
+            print(windowR)
             h = fl * np.tan(deltaTheta)
             d = np.sqrt((windowR ** 2) - (h ** 2))
             weight = 0.2 * np.arctan(d / fl) * np.pi / 180
@@ -579,7 +595,7 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
             geometric_cross_section = np.pi * (a / 2) ** 2
             m = m_sphere / mre
             if clickedM.get() == 'No Mineral':
-               m = m_sphere
+               m = mre
                qext, qsca, qback, g = miepython.mie(m, x)
             else:
                qext, qsca, qback, g = miepython.ez_mie(m_sphere, a, lam, mre)
@@ -615,7 +631,7 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
             #resI = []
 
             def rawQ(x):
-               tt = np.arange(eVec0[0], eVec0[1]+1, 1)
+               tt = np.arange(eVec0[0], eVec0[1]+0.1, .1)
                [k] = np.where(tt == x)
                if len(k) == 0:
                   newTheta = find_nearest(tt, x)
@@ -768,7 +784,7 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
             geometric_cross_section = np.pi * (a / 2) ** 2
             m = m_sphere / mre
             if clickedM.get() == 'No Mineral':
-               m = m_sphere
+               m = mre
                qext, qsca, qback, g = miepython.mie(m, x)
             else:
                qext, qsca, qback, g = miepython.ez_mie(m_sphere, a, lam, mre)
@@ -783,17 +799,11 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
 
             I = S11
             Q = S12
-            # print(I)
+
             sig = qsca * geometric_cross_section
             sigma.append(sig)
             aI.append(I)
             aQ.append(Q)
-
-            # if gVec0[2]==True:
-            # plt.subplot(3, 3, 3)
-
-
-            # plt.show()
          if gVec0[1] == True:
             ax2 = figs.add_subplot(332)
             subplots.append(ax2)
@@ -803,57 +813,65 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
             ax2.set_title("Scattering Cross Section")
             # plt.legend(loc='upper left')
 
-         def rawI(x):
-            tt = np.arange(eVec0[0], eVec0[1]+1, 1)
+         def rawI(x, pe):
+            tt = np.arange(-180, 180, 0.1)
+            #tt = np.arange(eVec0[0], eVec0[1]+0.1, 0.1)
             [k] = np.where(tt == x)
             if len(k) == 0:
                newTheta = find_nearest(tt, x)
                [k] = np.where(tt == newTheta)
-            y = float(aI[e][k])
+            y = float(aI[pe][k])
             return y
 
          resI = []
 
-         def rawQ(x):
-            tt = np.arange(eVec0[0], eVec0[1]+1, 1)
+         def rawQ(x, pe):
+            tt = np.arange(-180, 180, 0.1)
+            #tt = np.arange(eVec0[0], eVec0[1]+0.1, 0.1)
             [k] = np.where(tt == x)
             if len(k) == 0:
                newTheta = find_nearest(tt, x)
                [k] = np.where(tt == newTheta)
-            y = float(aQ[e][k])
+            y = float(aQ[pe][k])
+
             return y
 
          resQ = []
          ratioParticle = []
          resPlus = []
          resFrac = []
-         for e in range(0, len(aQ)):
+         tt = np.arange(eVec0[0], eVec0[1] + 0.1, 0.1)
+         for e in range(0, len(a0)):
             rQ = 0
             rI = 0
             rP = 0
             rPlus = 0
             rFrac = 0
-            tt = np.arange(eVec0[0], eVec0[1]+0.1, 0.1)
+            #tt = np.arange(eVec0[0], eVec0[1]+0.1, 0.1)
             for f in tt:
-               fl = eVec0[3]*1000  # Focal Length (200000 microns)
-               midTheta = (max(tt) + min(tt)) / 2  # Mid Point Theta (rad)
-               deltaTheta = np.abs(f - midTheta) * np.pi / 180  # Maximum delta (rad)
-               windowR = eVec0[2]*1000# Window radius
-               h = fl * np.tan(deltaTheta)
-               d = np.sqrt((windowR ** 2) - (h**2))
-               weight = 0.2*np.arctan(d/fl)*np.pi/180
+                fl = eVec0[3]*1000  # Focal Length (200000 microns)
+                midTheta = (max(tt) + min(tt)) / 2  # Mid Point Theta (rad)
+                deltaTheta = np.abs(f - midTheta) * np.pi / 180  # Maximum delta (rad)
+                windowR = eVec0[2]*1000# Window radius
+                h = fl * np.tan(deltaTheta)
+                d = np.sqrt((windowR ** 2) - (h**2))
+                #weight = 0.2*np.arctan(d/fl)*np.pi/180 # FIXME: What Does the Weight look like?
+                weight = 0.2 * np.arctan(d / fl) *180/np.pi
+                def WrawQ(x_q, pe_q):
+                   wQ = rawQ(x_q, pe_q) * weight
+                   return wQ
 
-               def WrawQ(x):
-                  return rawQ(x) * weight
+                resaQ = WrawQ(f, e)
+                #print(resaQ)
+                rQ = rQ + resaQ
 
-               resaQ=WrawQ(f)
-               rQ = rQ + resaQ
+                def WrawI(x_i, pe_i):
+                   wI = rawI(x_i, pe_i) * weight
+                   return wI
 
-               def WrawI(x):
-                  return rawI(x) * weight
+                resaI = WrawI(f, e)
+                rI = rI + resaI
 
-               resaI=WrawI(f)
-               rI = rI + resaI
             rP = (rQ - rI) / (rQ + rI)
             rPlus = (rQ + rI)
             rFrac = (rQ / rI)
@@ -967,7 +985,7 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
                geometric_cross_section = np.pi * (a / 2) ** 2
                m = m_sphere / mre
                if clickedM.get() == 'No Mineral':
-                  m = m_sphere
+                  m = mre
                   qext, qsca, qback, g = miepython.mie(m, x)
                else:
                   qext, qsca, qback, g = miepython.ez_mie(m_sphere, a, lam, mre)
@@ -988,14 +1006,12 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
                aI.append(I)
                aQ.append(Q)
 
-               # if gVec0[2]==True:
-               # plt.subplot(3, 3, 3)
             allI.append(aI)
             allQ.append(aQ)
             allS.append(sigma)
 
          def rawI(x):
-            tt = np.arange(168, 174, 1)
+            tt = np.arange(eVec0[0], eVec0[1]+0.1, .1)
             [k] = np.where(tt == x)
             if len(k) == 0:
                newTheta = find_nearest(tt, x)
@@ -1004,7 +1020,7 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
             return y
 
          def rawQ(x):
-            tt = np.arange(168, 174, 1)
+            tt = np.arange(eVec0[0], eVec0[1]+0.1, .1)
             [k] = np.where(tt == x)
             if len(k) == 0:
                newTheta = find_nearest(tt, x)
@@ -1090,6 +1106,7 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
                for j in range(len(a0)):
                   ax5.plot(theta, allI[i][j])
             ax5.set_xlim((0, 181))
+            #ax5.set_xlim((eVec0[0], eVec0[1]))
             ax5.axvline(x=eVec0[0], color="r")
             ax5.axvline(x=eVec0[1], color="r")
             ax5.set_xlabel("Scattering Angle (α°)")
@@ -1103,6 +1120,7 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
                for j in range(len(a0)):
                   ax6.plot(theta, allQ[i][j])
             ax6.set_xlim((0, 181))
+            #ax6.set_xlim((eVec0[0], eVec0[1]))
             ax6.axvline(x=eVec0[0], color="r")
             ax6.axvline(x=eVec0[0], color="r")
             ax6.set_xlabel("Scattering Angle (α°)")
@@ -1118,6 +1136,7 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
             ax3.set_ylabel("Detected Intensity")
             ax3.set_title("Intensity vs Particle Size")
             ax3.legend(ncol=2)
+
          if gVec0[6] == True:
             ax7 = figs.add_subplot(337)
             subplots.append(ax7)
@@ -1170,6 +1189,8 @@ def allG(m_sphere0, a0, lambda0, m_aerosol0, gVec0, eVec0):
    canvas.get_tk_widget().pack(side=tkinter.BOTTOM, fill=tkinter.BOTH, expand=True)
    saveB = tkinter.Button(newWindow, text="Save", command=saveCallBack)
    saveB.pack()
+
+#Saving the Figures
 def saveCallBack():
    # Save the full figure...
    figs.savefig("full_figure.png")
@@ -1179,6 +1200,7 @@ def saveCallBack():
       extent = subplots[i].get_window_extent().transformed(figs.dpi_scale_trans.inverted())
       figs.savefig('ax{}_figure.png'.format(i), bbox_inches=extent.expanded(1.3, 1.3))
 
+#Runing the Simulation
 def goCallBack():
    global gVector, subplots
    sVector = [sMineral, aS, sWavelen,sAerosol]
@@ -1194,13 +1216,16 @@ def goCallBack():
    # canvas = FigureCanvasTkAgg(f, newWindow)
    subplots = []
    allG(sVector[0], sVector[1], sVector[2], sVector[3], gVector, eVector)
+   print('Lab Setup Values: (%s,%s,%s,%s)' % (eVector[0],eVector[1],eVector[2],eVector[3]))
    # canvas.get_tk_widget().pack(side=tkinter.BOTTOM, fill=tkinter.BOTH, expand=True)
    saveB.pack()
 
+
+# Run and Save Buttons
 B = tkinter.Button(frame4, text ="Run", command = goCallBack)
 saveB = tkinter.Button(frame4, text="Save", command=saveCallBack)
 
-
+# Choosing Graph options
 CheckVar1 = IntVar()
 CheckVar2 = IntVar()
 CheckVar3 = IntVar()
